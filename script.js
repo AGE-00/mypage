0cdd353a92a7e3dede56eda22876fc89e3c9b292
AGEAGE <115388084+AGE-00@users.noreply.github.com>
2024-07-10 15:55:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-window.onload = function () {
-  document.getElementById("spread").animate(
-    {
-      height: 0,
-      opacity: 0.1
-    },
-    {
-      fill: "forwards",
-      duration: 1000,
-      easing: "ease-in"
-    }
-  );
-};
+
