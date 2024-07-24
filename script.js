4f217da738785ca907e2996031e39a3eeb47f170
age <riku.rtx3090.jp@gmail.com>
2024-07-24 13:34:24 +0900

    お気持ちを追加

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   AOS.init({
-    duration: 1000, // アニメーションの持続時間（ミリ秒）
+    duration: 100, // アニメーションの持続時間（ミリ秒）
     once: true, // スクロールダウン時のアニメーションを一度だけ実行
   });
 });
